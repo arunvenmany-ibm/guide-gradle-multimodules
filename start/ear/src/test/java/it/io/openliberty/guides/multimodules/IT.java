6944117dfc227e698a81e3fbf6064eb78326db31
Arun Venmany <Arun.Kumar.V.N@ibm.com>
2024-11-29 10:13:10 +0530

    Guide initial commit
    Signed-off-by: Arun Venmany <Arun.Kumar.V.N@ibm.com>

---
+++ start/ear/src/test/java/it/io/openliberty/guides/multimodules/IT.java
@@ -0,0 +1,25 @@
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2017, 2024 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+// end::copyright[]
+package it.io.openliberty.guides.multimodules;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
+
+public class IT {
+}
